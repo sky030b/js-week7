22dab95ce3631a4d907346a5bbcd87224ed4d0f7
sky030b <sky030blue030@gmail.com>
2023-11-24 16:59:59 +0800

    add rendering chart func. and modify some details

--- all.js
+++ all.js
@@ -1,12 +1,18 @@
+console.clear();
 let data = [];
 
 axios.get("https://raw.githubusercontent.com/hexschool/js-training/main/travelApi.json")
   .then((res) => {
     data = res.data.data;
     showSpots();
+
+    let areaObj = getAreaObj(data);
+    let areaArr = objToArr(areaObj);
+    renderChart(areaArr);
+    // renderChart(objToArr(getAreaObj(data)));
   })
-  .catch((e) => {
-    console.log(e);
+  .catch((error) => {
+    console.log(error);
   })
 
 const addTicketForm = document.querySelector(".addTicket-form");
@@ -20,6 +26,7 @@ const searchResult = document.querySelector("#searchResult-text");
 const ticketCardAll = document.querySelector(".ticketCard-area");
 const cantFindArea = document.querySelector(".cantFind-area");
 
+// for controling the alert message
 alerts.forEach((item, index) => {
   item.style.display = "none";
   inputs[index].addEventListener("input", function (e) {
@@ -31,41 +38,7 @@ alerts.forEach((item, index) => {
   })
 })
 
-function addSpot() {
-  let alertText = '';
-
-  if (!inputs[0].value) { alertText += "套票名稱請勿留空！<br>" };
-  if (!inputs[1].value) { alertText += "圖片網址請勿留空！<br>" };
-  if (!inputs[2].value) { alertText += "景點地區請擇一！<br>" };
-  if (!inputs[3].value) { alertText += "套票金額請勿留空！<br>" };
-  if (!inputs[4].value) { alertText += "套票組數請勿留空！<br>" };
-  if (!inputs[5].value) { alertText += "套票星級請勿留空！<br>" };
-  if (!inputs[6].value) { alertText += "套票描述請勿留空！<br>" };
-
-  if (alertText) {
-    showAlertText.innerHTML = alertText;
-  } else {
-    showAlertText.innerHTML = "";
-
-    let obj = {
-      "id": data.length,
-      "name": inputs[0].value,
-      "imgUrl": inputs[1].value,
-      "area": inputs[2].value,
-      "price": inputs[3].value,
-      "group": inputs[4].value,
-      "rate": inputs[5].value,
-      "description": inputs[6].value
-    };
-    data.push(obj);
-    regionSearch.value = "";
-    showSpots();
-    addTicketForm.reset();
-  }
-}
-
-addTicketBtn.addEventListener("click", addSpot);
-
+// for show ths spots
 function showSpots() {
   cantFindArea.style.display = "none";
 
@@ -125,3 +98,74 @@ function showSpots() {
 
 regionSearch.addEventListener("change", showSpots);
 
+// for add new spot
+function addSpot() {
+  let alertText = '';
+
+  if (!inputs[0].value) { alertText += "套票名稱請勿留空！<br>" };
+  if (!inputs[1].value) { alertText += "圖片網址請勿留空！<br>" };
+  if (!inputs[2].value) { alertText += "景點地區請擇一！<br>" };
+  if (!inputs[3].value) { alertText += "套票金額請勿留空！<br>" };
+  if (!inputs[4].value) { alertText += "套票組數請勿留空！<br>" };
+  if (!inputs[5].value) { alertText += "套票星級請勿留空！<br>" };
+  if (!inputs[6].value) { alertText += "套票描述請勿留空！<br>" };
+
+  if (alertText) {
+    showAlertText.innerHTML = alertText;
+  } else {
+    showAlertText.innerHTML = "";
+
+    let obj = {
+      "id": data.length,
+      "name": inputs[0].value,
+      "imgUrl": inputs[1].value,
+      "area": inputs[2].value,
+      "price": +inputs[3].value,
+      "group": +inputs[4].value,
+      "rate": +inputs[5].value,
+      "description": inputs[6].value
+    };
+    data.push(obj);
+    regionSearch.value = "";
+    renderChart(objToArr(getAreaObj(data)));
+    showSpots();
+    addTicketForm.reset();
+  }
+}
+
+addTicketBtn.addEventListener("click", addSpot);
+
+// for rendering area summary chart
+function getAreaObj(data) {
+  let outputObj = {};
+  data.forEach((item) => {
+    if (item.area in outputObj) {
+      outputObj[item.area]++;
+    } else {
+      outputObj[item.area] = 1;
+    }
+  })
+  return outputObj;
+}
+
+function objToArr(obj) {
+  let keys = Object.keys(obj);
+  let outputArr = keys.map((key) => [key, obj[key]]);
+  return outputArr;
+}
+
+function renderChart(areaArr) {
+  const chart = c3.generate({
+    bindto: '#chart', // HTML 元素綁定
+    data: {
+      columns: areaArr, // 資料存放
+      type: 'donut',
+    },
+    donut: {
+      title: "套票地區比重"
+    },
+    size: {
+      height: 250
+    }
+  });
+}
